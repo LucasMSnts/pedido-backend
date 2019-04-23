349fbf4a480fdd500f75ca55bc88e015a25790ff
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-04-23 16:36:33 -0300

    Projeto criado

---
+++ src/main/java/com/lucasmauro/projetopedido/ProjetopedidoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmauro.projetopedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopedidoApplication.class, args);
+	}
+
+}
