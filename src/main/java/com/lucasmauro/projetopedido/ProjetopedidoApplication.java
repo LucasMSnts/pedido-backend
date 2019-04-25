c31434b848183086c399c59cce323c572b628c57
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-04-25 17:19:30 -0300

    Instanciando DaDos, Classe Produto e Associação: Muitos para muitos

--- src/main/java/com/lucasmauro/projetopedido/ProjetopedidoApplication.java
+++ src/main/java/com/lucasmauro/projetopedido/ProjetopedidoApplication.java
@@ -1,13 +1,49 @@
 package com.lucasmauro.projetopedido;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasmauro.projetopedido.dominio.Categoria;
+import com.lucasmauro.projetopedido.dominio.Produto;
+import com.lucasmauro.projetopedido.repositorios.CategoriaRepositorio;
+import com.lucasmauro.projetopedido.repositorios.ProdutoRepositorio;
+
 @SpringBootApplication
-public class ProjetopedidoApplication {
+public class ProjetopedidoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepositorio categoriaRepo;
+	@Autowired
+	private ProdutoRepositorio produtoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetopedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
