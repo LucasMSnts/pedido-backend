3359f0c6fbe2b142952f75de3363c9aacaeee79b
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-04-30 16:09:54 -0300

    Ajustes no endpoint /categoria/{id}

---
+++ src/main/java/com/lucasmauro/projetopedido/recursos/excecao/RecursoExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.lucasmauro.projetopedido.recursos.excecao;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lucasmauro.projetopedido.servicos.excecoes.ObjectNotFoundException;
+
+@ControllerAdvice
+public class RecursoExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<ErroPadrao> objetoNaoLocalizado(ObjectNotFoundException e, HttpServletRequest request){
+		
+		ErroPadrao erro = new ErroPadrao(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+}
