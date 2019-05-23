60a4318044c878136d635ab1d5db0de00e9da8ca
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-05-23 16:50:29 -0300

    Endpoint /pedidos/{id}

---
+++ src/main/java/com/lucasmauro/projetopedido/recursos/PedidoRecurso.java
@@ -0,0 +1,25 @@
+package com.lucasmauro.projetopedido.recursos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasmauro.projetopedido.dominio.Pedido;
+import com.lucasmauro.projetopedido.servicos.PedidoServico;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoRecurso {
+	
+	@Autowired
+	private PedidoServico servico;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {			
+		Pedido obj = servico.buscar(id);
+		return ResponseEntity.ok().body(obj);		
+	}
+}
---
+++ src/main/java/com/lucasmauro/projetopedido/servicos/PedidoServico.java
@@ -0,0 +1,23 @@
+package com.lucasmauro.projetopedido.servicos;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasmauro.projetopedido.dominio.Pedido;
+import com.lucasmauro.projetopedido.repositorios.PedidoRepositorio;
+import com.lucasmauro.projetopedido.servicos.excecoes.ObjectNotFoundException;
+
+@Service
+public class PedidoServico {
+
+	@Autowired
+	private PedidoRepositorio repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}	
+}
