c2ccfdacb6b9492d555418ff54a5fab2dcb8f439
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-07-12 16:33:26 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/lucasmauro/projetopedido/recursos/ClienteRecurso.java
+++ src/main/java/com/lucasmauro/projetopedido/recursos/ClienteRecurso.java
@@ -1,13 +1,22 @@
 package com.lucasmauro.projetopedido.recursos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasmauro.projetopedido.dominio.Cliente;
+import com.lucasmauro.projetopedido.dto.ClienteDTO;
 import com.lucasmauro.projetopedido.servicos.ClienteServico;
 
 @RestController
@@ -19,7 +28,39 @@ public class ClienteRecurso {
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {			
-		Cliente obj = servico.buscar(id);
+		Cliente obj = servico.find(id);
 		return ResponseEntity.ok().body(obj);		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
+		Cliente obj = servico.fromDTO(objDTO);
+		obj.setId(id);
+		obj = servico.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		servico.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {			
+		List<Cliente> lista = servico.findAll();
+		List<ClienteDTO> listaDTO = lista.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);		
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="pagina", defaultValue="0") Integer page, 
+			@RequestParam(value="linhas", defaultValue="24") Integer linhasPorPaginas, 
+			@RequestParam(value="ordenar", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direcao", defaultValue="ASC") String direcao) {			
+		Page<Cliente> lista = servico.findPage(page, linhasPorPaginas, orderBy, direcao);
+		Page<ClienteDTO> listaDTO = lista.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listaDTO);		
 	}
 }
--- src/main/java/com/lucasmauro/projetopedido/servicos/PedidoServico.java
+++ src/main/java/com/lucasmauro/projetopedido/servicos/PedidoServico.java
@@ -15,7 +15,7 @@ public class PedidoServico {
 	@Autowired
 	private PedidoRepositorio repo;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
