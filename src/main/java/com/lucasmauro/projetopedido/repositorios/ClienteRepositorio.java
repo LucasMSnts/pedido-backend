c5f242ed6176315736f31d65d01c86f78ad2b239
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-07-18 10:49:12 -0300

    Validação customizada: email não repetido na inserção do Cliente

--- src/main/java/com/lucasmauro/projetopedido/repositorios/ClienteRepositorio.java
+++ src/main/java/com/lucasmauro/projetopedido/repositorios/ClienteRepositorio.java
@@ -2,10 +2,13 @@ package com.lucasmauro.projetopedido.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lucasmauro.projetopedido.dominio.Cliente;
 
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
