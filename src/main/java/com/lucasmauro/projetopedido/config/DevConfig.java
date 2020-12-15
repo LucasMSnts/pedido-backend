49d071d1d1a61863de90875301c7368cc1d85f76
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-12-14 23:53:26 -0300

    Criar profile de desenvolvimento

---
+++ src/main/java/com/lucasmauro/projetopedido/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.lucasmauro.projetopedido.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucasmauro.projetopedido.servicos.DBServico;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBServico dbServico;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbServico.instantiateTestDatabase();
+		return true;
+	}
+}
