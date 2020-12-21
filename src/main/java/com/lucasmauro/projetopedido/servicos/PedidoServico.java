5d6d6de11c1ec5add07b5d5ac999ae5015d55542
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-12-21 00:19:01 -0300

    Implementar toString de Pedido

--- src/main/java/com/lucasmauro/projetopedido/servicos/PedidoServico.java
+++ src/main/java/com/lucasmauro/projetopedido/servicos/PedidoServico.java
@@ -34,6 +34,9 @@ public class PedidoServico {
 	@Autowired
 	private ItemPedidoRepositorio itemPedidoRepositorio;
 	
+	@Autowired
+	private ClienteServico clienteServico;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -44,6 +47,7 @@ public class PedidoServico {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteServico.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -54,10 +58,12 @@ public class PedidoServico {
 		pagamentoRepositorio.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoServico.find(ip.getProduto().getId()).getPreco()); 
+			ip.setProduto(produtoServico.find(ip.getProduto().getId())); 
+			ip.setPreco(ip.getProduto().getPreco()); 
 			ip.setPedido(obj);
 		}
 		itemPedidoRepositorio.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
