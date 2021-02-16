05c1da457454ed06948a1887586e34d984a4748a
Lucas Mauro <lucas_de_mauro@hotmail.com>
2021-02-16 12:53:42 -0300

    Salvar primeiro arquivo no S3

--- src/main/java/com/lucasmauro/projetopedido/ProjetopedidoApplication.java
+++ src/main/java/com/lucasmauro/projetopedido/ProjetopedidoApplication.java
@@ -1,11 +1,17 @@
 package com.lucasmauro.projetopedido;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasmauro.projetopedido.servicos.S3Servico;
+
 @SpringBootApplication
 public class ProjetopedidoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Servico s3Servico;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetopedidoApplication.class, args);
@@ -13,7 +19,7 @@ public class ProjetopedidoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-				
+		s3Servico.uploadFile("C:\\Users\\lucas\\Pictures\\Papyros2.jpg");		
 	}
 
 }
